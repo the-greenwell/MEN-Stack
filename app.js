09a5a36ed843c379979475c62287bc9d6546d59f
the-greenwell <ant.greenwell2@gmail.com>
2019-10-22 09:59:12 -0700

    simple views

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine','pug');
+
 app.get('/', function(req,res){
-  res.send('This is the home-page');
+  res.render('index',{title:'Home',message:'Welcome to the Home Page!'});
 });
 
 app.route('/about').get(function(req,res){
-  res.send('This is the about-page');
+  res.render('about',{title:'About',message:'Lorem Ipsem!'});
 })
 
 app.route('/contact').get(function(req,res){
-  res.send('This is the contact-page');
+  res.render('contact',{title:'Contact',message:'Contact me here!'});
 });
 
 var server = app.listen(3000,function(){
